b1dc53450844a9c20c6101b810c1d078df304e7a
Crowdbotics <team@crowdbotics.com>
2021-08-18 17:04:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "andriekambaconsulti_29915Identifier",
+  url: "https://andriekambaconsulti_29915Identifier.botics.co",
+  api: "https://andriekambaconsulti_29915Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
